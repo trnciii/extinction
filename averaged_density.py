1a762e5890d995138b0896bf31336cc4de760946
trnciii <doityan223@gmail.com>
2022-06-01 09:23:22 +0900

    go numpythonic

--- averaged_density.py
+++ averaged_density.py
@@ -2,23 +2,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-whitenoise = lambda rng, n: rng.random(n)
+n = 100
 
+rngs = [np.random.default_rng(seed=mu) for mu in range(50)]
+timeline = np.linspace(1/n, 1, n)
 
-sigmas = []
-sigmas_averaged = []
-
-for mu in range(50):
-	n = 100
-	rng = np.random.default_rng(seed=mu)
-
-	t = np.linspace(1/n, 1, n)
-	sigmas.append(whitenoise(rng, n))
-	sigmas_averaged.append(np.cumsum(sigmas[-1])/t)
-
-
-	# print(sigma)
-	# print(sigma_averaged)
+sigmas = np.array([rng.random(n) for rng in rngs])
+sigmas_averaged = np.cumsum(sigmas, axis=1)/timeline
 
 
 figs = {}
@@ -30,15 +20,15 @@ line_cloud = ('#77777777', 0, 1)
 figs['sigma'] = plt.figure()
 for i in range(len(sigmas)):
 	c, order, w = line_top if i == 0 else line_cloud
-	plt.plot(t, sigmas[i], color=c, linewidth=w, zorder=order)
+	plt.plot(timeline, sigmas[i], color=c, linewidth=w, zorder=order)
 
 figs['sigma_averaged'] = plt.figure()
 for i in range(len(sigmas_averaged)):
 	c, order, w = line_top if i == 0 else line_cloud
-	plt.plot(t, sigmas_averaged[i], color=c, linewidth=w, zorder=order)
+	plt.plot(timeline, sigmas_averaged[i], color=c, linewidth=w, zorder=order)
 
 
-shown = {'sigma_averaged'}
+shown = {'sigma', 'sigma_averaged'}
 for f in [v for k, v in figs.items() if k not in shown]:
 	plt.close(f)
 plt.show()
