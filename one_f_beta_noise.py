9da51d63bd9824d8988340860f5d7df5c89c8cc5
trnciii <doityan223@gmail.com>
2022-06-24 17:10:45 +0900

    draw noise cloud and histogram

--- one_f_beta_noise.py
+++ one_f_beta_noise.py
@@ -2,30 +2,56 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def one_f_beta(n, beta, rng=np.random.default_rng()):
-	kmax = n//2
+def one_f_beta(n, beta, rngs=[np.random.default_rng(seed=0)]):
+	if type(rngs) is not list:
+		rngs = [rngs]
 
-	f = np.linspace(1, kmax, kmax)/(2*np.pi)
+	kmax = n//2
+	seqs = len(rngs)
+
+
+	f = np.tile( np.linspace(1, kmax, kmax), (seqs, 1) ) / (2*np.pi)
 	C = 1/np.abs(f**beta)
 
-	phase = 2*np.pi*rng.random(kmax)
+	phase = np.array([2*np.pi*rng.random((kmax)) for rng in rngs])
 	Cpos = C*np.exp(1j*phase)
 	Cneg = np.flip(np.conj(Cpos))
 
-	noise = np.fft.ifft(np.concatenate(([0], Cpos, Cneg)))
+	C = np.concatenate((np.zeros((seqs, 1)), Cpos, Cneg), axis=1)
+	noise = np.fft.ifft(C)
 
-	return noise.real[:n]
+	return noise.real[:, :n]
 
 
 
 if __name__ == '__main__':
 	n = 1000
 	beta = 1
+	seqs = 200
 
-	rng = np.random.default_rng(0)
+	rngs = [np.random.default_rng(seed=i) for i in range(seqs)]
+	noises = one_f_beta(n, beta, rngs)
 
-	noise = one_f_beta(n, beta, rng)
+
+	fig, ax = plt.subplots(1, 2)
+
+	# color, order, line width
+	line_top = 'salmon', 1, 2
+	line_cloud = '#77777777', 0, 1
 
 	timeline = np.linspace(0, 1, n)
-	plt.plot(timeline, noise)
+	for i in range(len(noises)):
+		c, order, w = line_top if i == 0 else line_cloud
+		ax[0].plot(timeline, noises[i], color=c, linewidth=w, zorder=order)
+	ax[0].set_position((0.1, 0.05, 0.7, 0.95))
+
+
+	ax[1].hist(noises.reshape((-1)), bins=20, orientation='horizontal')
+	ax[1].set_position((0.8, 0.05, 0.2, 0.95))
+	# ax[1].set_box_aspect(4)
+	ax[1].get_xaxis().set_visible(False)
+	ax[1].get_yaxis().set_visible(False)
+
+
+
 	plt.show()
