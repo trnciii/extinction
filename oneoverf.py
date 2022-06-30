f0b2c047014088a01c05ce0907a886718458b13d
trnciii <doityan223@gmail.com>
2022-06-30 14:09:59 +0900

    update 1/f noise
    rename
    generate a single sequence

--- oneoverf.py
+++ oneoverf.py
@@ -4,25 +4,20 @@ from matplotlib import pyplot as plt
 import figurateur
 
 
-def one_f_beta(n, beta, rngs=[np.random.default_rng(seed=0)]):
-	if type(rngs) is not list:
-		rngs = [rngs]
-
+def sequence(n, beta, rng):
 	kmax = n//2
-	seqs = len(rngs)
 
-
-	f = np.tile( np.linspace(1, kmax, kmax), (seqs, 1) ) / (2*np.pi)
+	f = np.linspace(1, kmax, kmax)/(2*np.pi)
 	C = 1/np.abs(f**beta)
 
-	phase = np.array([2*np.pi*rng.random((kmax)) for rng in rngs])
+	phase = np.array(2*np.pi*rng.random(kmax))
 	Cpos = C*np.exp(1j*phase)
-	Cneg = np.flip(np.conj(Cpos), axis=1)
+	Cneg = np.flip(np.conj(Cpos))
 
-	C = np.concatenate((np.zeros((seqs, 1)), Cpos, Cneg), axis=1)
+	C = np.concatenate(([0], Cpos, Cneg))
 	noise = np.fft.ifft(C)
 
-	return noise.real[:, :n]
+	return noise.real[:n]
 
 
 
@@ -32,7 +27,7 @@ if __name__ == '__main__':
 	seqs = 100
 
 	rngs = [np.random.default_rng(seed=i) for i in range(seqs)]
-	noises = one_f_beta(n, beta, rngs)
+	noises = np.array([sequence(n, beta, rng) for rng in rngs])
 
 
 	figs = {}
