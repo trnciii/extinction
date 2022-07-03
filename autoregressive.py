5269b9f54b94dab09d39d47cb3b10e8e72d32a66
trnciii <doityan223@gmail.com>
2022-07-03 23:20:47 +0900

    use gaussian noise. keep |k| <= 1 for stationarity

--- autoregressive.py
+++ autoregressive.py
@@ -1,29 +1,14 @@
 import numpy as np
 
 def sequence(n, k, rng):
-	if k>=1: k = 0.99
-	if k<=-1: k = -0.99
-
-	mean = lim_mean(k)
+	if k>1: k = 1
+	if k<-1: k = -1
 
 	ret = np.empty(n)
-	ret[0] = rng.uniform() * 2*mean
+	ret[0] = rng.normal()
 	for i in range(n-1):
-		ret[i+1] = k*ret[i] + rng.uniform()*np.sqrt(1-k*k)
+		ret[i+1] = k*ret[i] + rng.normal()
 	return ret
-
-def lim_mean(k):
-	return np.sqrt(1-k*k) / (2*(1-k))
-
-def lim_std(k):
-	return np.sqrt((1-k*k)/12)/(1-k)
-
-def mean(x0, n, k):
-	term1 = (x0 * (1-k**n)) / (n*(1-k))
-	term2 = lim_mean(k)
-	term3 = (np.sqrt(1-k*k) * (1-k**n)) / (2*n*(1-k)*(1-k))
-
-	return term1 + term2 + term3
 
 
 if __name__ == '__main__':
