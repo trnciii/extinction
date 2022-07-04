7c3bc2ff1de5fb1d2b728ab8dffc27dc7d955b7c
trnciii <doityan223@gmail.com>
2022-07-04 20:01:29 +0900

    plot autocorrelation

---
+++ autocorrelation.py
@@ -0,0 +1,44 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import sys
+
+import autoregressive
+import oneoverf
+
+import figurateur
+
+
+def acf(x, window=None):
+	N = len(x)
+	if window == None: window = N//2
+
+	ret = np.array([np.dot(x[:window], x[j:j+window]) for j in range(N-window)])
+
+	return ret/ret[0]
+
+
+figs = {}
+
+for generator in [autoregressive, oneoverf]:
+
+	n = 1000
+	num_k = 7
+
+	figs[generator.name], ax = plt.subplots(num_k, 2, tight_layout=True, figsize=(19.2, 10.8))
+
+	rng = np.random.default_rng(seed=1234)
+
+	for k, (ax_nosie, ax_ac) in zip(np.linspace(-1, 1, num_k), ax):
+		noise = generator.sequence(n, k, rng)
+		ac = acf(noise)
+
+		ax_nosie.plot(np.linspace(1, n, n), noise, linewidth=0.6)
+		ax_nosie.set_title('k = {:.3}'.format(k), loc='left')
+		ax_ac.plot(np.linspace(1, len(ac), len(ac)), ac, linewidth=0.6)
+
+
+figurateur.save(figs, prefix='autocorr_')
+
+if not '-b' in sys.argv:
+	figurateur.show(figs)
+
