cd97f60adec6eebba120e55d777df3cac6738afa
trnciii <doityan223@gmail.com>
2022-07-07 03:42:56 +0900

    prevent unwanted run

--- autocorrelation.py
+++ autocorrelation.py
@@ -1,11 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
-import sys
-
-import autoregressive
-import oneoverf
-
-import figurateur
 
 
 def acf(x, window=None):
@@ -14,28 +7,38 @@ def acf(x, window=None):
 	return npcorr/npcorr[0]
 
 
-figs = {}
+if __name__ == '__main__':
+	from matplotlib import pyplot as plt
+	import sys
 
-for generator in [autoregressive, oneoverf]:
+	import autoregressive
+	import oneoverf
 
-	n = 1000
-	num_k = 7
-
-	figs[generator.name], ax = plt.subplots(num_k, 2, tight_layout=True, figsize=(19.2, 10.8))
-
-	rng = np.random.default_rng(seed=1234)
-
-	for k, (ax_nosie, ax_ac) in zip(np.linspace(-1, 1, num_k), ax):
-		noise = generator.sequence(n, k, rng)
-		ac = acf(noise)
-
-		ax_nosie.plot(np.linspace(1, n, n), noise, linewidth=0.6)
-		ax_nosie.set_title('k = {:.3}'.format(k), loc='left')
-		ax_ac.plot(np.linspace(1, len(ac), len(ac)), ac, linewidth=0.6)
+	import figurateur
 
 
-figurateur.save(figs, prefix='autocorr_')
+	figs = {}
 
-if not '-b' in sys.argv:
-	figurateur.show(figs)
+	for generator in [autoregressive, oneoverf]:
+
+		n = 1000
+		num_k = 7
+
+		figs[generator.name], ax = plt.subplots(num_k, 2, tight_layout=True, figsize=(19.2, 10.8))
+
+		rng = np.random.default_rng(seed=1234)
+
+		for k, (ax_nosie, ax_ac) in zip(np.linspace(-1, 1, num_k), ax):
+			noise = generator.sequence(n, k, rng)
+			ac = acf(noise)
+
+			ax_nosie.plot(range(n), noise, linewidth=0.6)
+			ax_nosie.set_title('k = {:.3}'.format(k), loc='left')
+			ax_ac.plot(range(ac.shape[0]), ac, linewidth=0.6)
+
+
+	figurateur.save(figs, prefix='autocorr_')
+
+	if not '-b' in sys.argv:
+		figurateur.show(figs)
 
--- averaged_density.py
+++ averaged_density.py
@@ -1,32 +1,34 @@
-import numpy as np
-from matplotlib import pyplot as plt
-import os
+if __name__ == '__main__':
+	import numpy as np
+	from matplotlib import pyplot as plt
+	import os
 
-import autoregressive, oneoverf
-import figurateur
+	import autoregressive, oneoverf
+	import figurateur
 
-noise = autoregressive
-noise = oneoverf
-
-n = 500
-k_or_beta = 0
-
-rngs = [np.random.default_rng(seed=mu) for mu in range(50)]
-timeline = np.linspace(1, n, n)
-
-sigmas = np.array([noise.sequence(n, k_or_beta, rng) for rng in rngs])
-sigmas_averaged = np.cumsum(sigmas, axis=1)/timeline
+	noise = autoregressive
+	noise = oneoverf
 
 
-figs = {}
+	n = 500
+	k_or_beta = 0
 
-figs.update(figurateur.inspect_noise(sigmas))
+	rngs = [np.random.default_rng(seed=mu) for mu in range(50)]
+	timeline = np.linspace(1, n, n)
 
-figs['sigma_averaged'], ax = plt.subplots(1,1)
-figurateur.cloud(ax, timeline, sigmas_averaged)
-ax.set_xscale('log')
+	sigmas = np.array([noise.sequence(n, k_or_beta, rng) for rng in rngs])
+	sigmas_averaged = np.cumsum(sigmas, axis=1)/timeline
 
 
-pre = 'ar_' if noise is autoregressive else '1_f_'
-figurateur.save(figs, prefix=pre)
-figurateur.show(figs, exclude={'powers'})
+	figs = {}
+
+	figs.update(figurateur.inspect_noise(sigmas))
+
+	figs['sigma_averaged'], ax = plt.subplots(1,1)
+	figurateur.cloud(ax, timeline, sigmas_averaged)
+	ax.set_xscale('log')
+
+
+	pre = 'ar_' if noise is autoregressive else '1_f_'
+	figurateur.save(figs, prefix=pre)
+	figurateur.show(figs, exclude={'powers'})
