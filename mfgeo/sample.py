ab60d97b81eab11357fca72ae2230d714c8fd6c8
trnciii <doityan223@gmail.com>
2022-08-13 00:22:01 +0900

    update sampling

--- mfgeo/sample.py
+++ mfgeo/sample.py
@@ -1,23 +1,35 @@
 import numpy as np
 
 def cosined_hemisphere(u):
+	assert u.ndim == 2 and u.shape[1] == 2
+
 	u1 = 2*np.pi*u[:, 1]
 	r = np.sqrt(u[:, 0])
 	hemi = np.empty((u.shape[0], 3))
 	hemi[:, 0] = r*np.cos(u1)
 	hemi[:, 1] = r*np.sin(u1)
 	hemi[:, 2] = np.sqrt(1-u[:, 0])
-	return hemi
+	return hemi, hemi[:, 2]/np.pi
+
+def cosined_hemisphere_angle(u):
+	assert u.ndim == 1
+	return np.arcsin(np.sqrt(u))
 
 
 def uniform_hemisphere(u):
+	assert u.ndim == 2 and u.shape[1] == 2
+
 	u1 = 2*np.pi*u[:, 1]
 	r = np.sqrt(1 - u[:, 0]**2)
 	uni = np.empty((u.shape[0], 3))
 	uni[:, 0] = r*np.cos(u1)
 	uni[:, 1] = r*np.sin(u1)
 	uni[:, 2] = u[:, 0]
-	return uni
+	return uni, np.array([1/(2*np.pi)]*u.shape[0])
+
+def uniform_hemisphere_angle(u):
+	assert u.ndim == 1
+	return np.arccos(u)
 
 
 if __name__ == "__main__":
@@ -26,15 +38,37 @@ if __name__ == "__main__":
 
 
 	rng = np.random.default_rng()
-	u = rng.uniform(size=(5000, 2))
+	u = rng.uniform(size=(10000, 2))
 
-	uni = uniform_hemisphere(u)
-	cos = cosined_hemisphere(u)
+	uni, p_u = uniform_hemisphere(u)
+	cos, p_c = cosined_hemisphere(u)
 
-	z = uni[:, 2]
+	uni_a = uniform_hemisphere_angle(u[:,0])
+	cos_a = cosined_hemisphere_angle(u[:,0])
 
-	for alpha in np.linspace(0, 1, 11):
-		print(f'{alpha:.2f}', np.mean(mfgeo.ggx.ndf(z, alpha)*z) * (2*np.pi))
+	z_u = uni[:, 2]
+	z_c = cos[:, 2]
+
+	print('on sphere')
+	for alpha in np.linspace(0.1, 1, 10):
+		sum_u = np.mean(mfgeo.ggx.ndf(np.arccos(z_u), alpha)*z_u) * 2*np.pi
+		sum_c = np.mean(mfgeo.ggx.ndf(np.arccos(z_c), alpha)) * np.pi
+		print(f'{alpha:.1f} {sum_u:.10f} {sum_c:.10f} | {np.abs(1-sum_u):.10f} {np.abs(1-sum_c):.10f}')
+
+	print()
+
+	a_u = uniform_hemisphere_angle(u.reshape((-1)))
+	a_c = cosined_hemisphere_angle(u.reshape((-1)))
+
+	print('angle')
+	for alpha in np.linspace(0.1, 1, 10):
+		sum_u = np.mean(mfgeo.ggx.ndf(a_u, alpha)*np.cos(a_u)) * 2*np.pi
+		sum_c = np.mean(mfgeo.ggx.ndf(a_c, alpha)) * np.pi
+		print(f'{alpha:.1f} {sum_u:.10f} {sum_c:.10f} | {np.abs(1-sum_u):.10f} {np.abs(1-sum_c):.10f}')
+
+
+	print(end='', flush=True)
+	exit()
 
 
 	fig, ax = plt.subplots(1,1, subplot_kw=dict(projection='3d'))
