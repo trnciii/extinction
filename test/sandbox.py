d1693de029b96f97c50d1b3d6b027394b4ad7916
trnciii <doityan223@gmail.com>
2022-09-02 16:27:23 +0900

    update sandbox

--- test/sandbox.py
+++ test/sandbox.py
@@ -1,20 +1,32 @@
 from mfgeo import noise, figurateur
 import numpy as np
 from matplotlib import pyplot as plt
+import os
 
-rngs = [np.random.default_rng(seed=mu) for mu in range(10)]
+outdir = 'result/sandbox'
+os.makedirs(outdir, exist_ok=True)
 
-for noisetype in [noise.oneoverf]:
-	n_range = [10**e for e in range(3, 8)]
-	fig, axes = plt.subplots(len(n_range), 1, figsize=(20, 10), constrained_layout=True)
 
-	for n, ax in zip(n_range, axes):
-		height = np.array([noisetype.sequence(n, 1, rng) for rng in rngs])
+figs = {}
+generator = noise.oneoverf
+n_range = [10**e for e in range(3, 8)]
+figs[generator.name], axes = plt.subplots(len(n_range), 1, figsize=(20, 10), constrained_layout=True)
 
-		size = 500
-		sliced = height[:, :size]*n/1000
-		figurateur.cloud(ax, np.linspace(1, size, size), sliced)
+for n, ax in zip(n_range, axes):
+	rngs = [np.random.default_rng(seed=mu) for mu in range(10)]
 
-plt.savefig('result/sadbox.png')
-plt.show()
+	height = np.array([generator.sequence(n, 1, rng) for rng in rngs])
+
+	print(f'type={generator.name} {n=} min={np.amin(height)} max={np.amax(height)} std={np.std(height)}')
+	print(flush=True)
+
+	size = height.shape[1]
+	# size = 500
+	sliced = height[:, -size:]
+	figurateur.cloud(ax, np.linspace(1, size, size), sliced)
+
+plt.savefig(os.path.join(outdir, f'{generator.name}.png'.replace('/', '-')))
+
+figurateur.save(figs, out_dir=outdir)
+figurateur.show(figs)
 
