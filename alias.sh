79469483e8199590b42f61e7bd2a67a8c68dc6f6
trnciii <doityan223@gmail.com>
2022-09-10 03:18:30 +0900

    update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,4 +1,13 @@
-ROOT=$(dirname "$(realpath "$BASH_SOURCE")")
+ROOT=$(dirname "$(readlink -f "$BASH_SOURCE")")
 echo "root: $ROOT"
+echo "os: ${OSTYPE}"
 
-alias install='python3 -m pip install -e $ROOT'
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	alias install='python3 -m pip install -e $ROOT'
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+	alias install='python3 setup.py develop --user'
+elif [[ "$OSTYPE" == "msys" ]]; then
+	alias install='python3 setup.py develop'
+else
+	echo 'failed to set install alias'
+fi
