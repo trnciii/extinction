fix freq normalization of 1/f noise

mfgeo/noise/oneoverf.py
@@ -8,7 +8,7 @@ def sequence(n, b, rng):
 
 	kmax = n//2
 
-	f = np.linspace(1, kmax, kmax)/(2*np.pi)
+	f = np.linspace(0.5, 1, kmax)/(2*np.pi)
 	C = 1/np.abs(f**b)
 
 	phase = np.array(2*np.pi*rng.random(kmax))

test/sandbox.py
@@ -8,24 +8,27 @@ outdir = path.out()
 
 figs = {}
 generator = noise.oneoverf
-n_range = [10**e for e in range(3, 8)]
+n_range = [10**e for e in range(3, 6)]
 figs[generator.name], axes = plt.subplots(len(n_range), 1, figsize=(20, 10), constrained_layout=True)
 
 for n, ax in zip(n_range, axes):
 	rngs = [np.random.default_rng(seed=mu) for mu in range(10)]
 
-	height = np.array([generator.sequence(n, 1, rng) for rng in rngs])
+	height = np.array([generator.sequence(n, 0.99, rng) for rng in rngs])
+	slope = np.diff(height)
 
-	print(f'type={generator.name} {n=} min={np.amin(height)} max={np.amax(height)} std={np.std(height)}')
+	target = height[:, :200]
+
+
+	print(f'type={generator.name} {n=} min={np.amin(target)} max={np.amax(target)} std={np.std(target)}')
 	print(flush=True)
 
-	size = height.shape[1]
+	size = target.shape[1]
 	# size = 500
-	sliced = height[:, -size:]
+	sliced = target[:, -size:]
 	figurateur.cloud(ax, np.linspace(1, size, size), sliced)
 
 plt.savefig(os.path.join(outdir, f'{generator.name}.png'.replace('/', '-')))
 
 figurateur.save(figs, out_dir=outdir)
 figurateur.show(figs)
-

test/slope_distribution.py
@@ -18,18 +18,17 @@ for n in [100, 1000, 10000]:
 	rngs = [np.random.default_rng(seed=mu) for mu in range(1000)]
 
 	# for beta in np.linspace(-0.99, 0.99, 3):
-	beta = -0.99
+	beta = 0.99
 
-	height = np.array([autoregressive.sequence(n, beta, rng) for rng in rngs])
+	height = np.array([oneoverf.sequence(n, beta, rng) for rng in rngs])
 	slope = np.diff(height)
 
 	hist, bin_edges = np.histogram(slope, bins='auto', density=True)
 
-
-	print(f'{hist.size=}')
-	print(hist)
-	print(f'{bin_edges.size=}')
-	print(bin_edges)
+	# print(f'{hist.size=}')
+	# print(hist)
+	# print(f'{bin_edges.size=}')
+	# print(bin_edges)
 
 	sample_slope = [-1, 0, 1]
 	print(f'prob={probability(sample_slope, slope)}')