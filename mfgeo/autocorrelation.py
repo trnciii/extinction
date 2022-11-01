c60d9b263aeb5138eef151104881e0e45144ae48
trnciii <doityan223@gmail.com>
2022-11-01 23:57:19 +0900

    add acf methods

--- mfgeo/autocorrelation.py
+++ mfgeo/autocorrelation.py
@@ -1,13 +1,53 @@
 import numpy as np
 
+def w_k(x):
+	size = 2**np.ceil(np.log2(2*len(x) - 1)).astype('int')
+	fft = np.fft.fft(x, size)
+	pwr = np.abs(fft)**2
+	corr = np.fft.ifft(pwr).real[:len(x)]
+	return corr/corr[0]
 
-def acf(x, window=None):
+
+def full(x):
+	corr = np.correlate(x, x, 'full')[len(x)-1:]
+	return corr/corr[0]
+
+
+def valid(x, window=None):
 	if window == None: window = len(x)//2
-	npcorr = np.correlate(x, x[:window], mode='valid')
-	return npcorr/npcorr[0]
+	corr = np.correlate(x, x[:window], mode='valid')
+	return corr/corr[0]
+
+
+def same(x):
+	corr = np.correlate(x, x, mode='same')[len(x)//2:]
+	return corr/corr[0]
+
+
+acf = full
+
+
+def runall():
+	from matplotlib import pyplot as plt
+
+	rng = np.random.default_rng(seed=0)
+	x = rng.random(100)
+	for name, f in [('w_k', w_k), ('full', full), ('valid', valid), ('same', same)]:
+		co = f(x)
+		print(name, len(co))
+		print(co)
+		plt.plot(range(len(co)), co, label=name)
+
+	print(flush=True)
+	plt.legend()
+	plt.show()
 
 
 if __name__ == '__main__':
+
+	runall()
+	exit()
+
 	from matplotlib import pyplot as plt
 	import sys
 
