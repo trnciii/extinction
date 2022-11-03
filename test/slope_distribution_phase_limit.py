dfb3719fc8b74581eedac53297015016a338c99f
trnciii <doityan223@gmail.com>
2022-11-03 23:10:52 +0900

    fix field length

--- test/slope_distribution_phase_limit.py
+++ test/slope_distribution_phase_limit.py
@@ -14,39 +14,41 @@ def probability(x, sample):
 		0
 	)
 
-phase = (0, 1*np.pi)
+phase = (0, 0.5*np.pi)
 
-for n in [100, 1000, 10000, 100000]:
-	rngs = [np.random.default_rng(seed=mu) for mu in range(1000)]
+n = 10000
+rngs = [np.random.default_rng(seed=mu) for mu in range(1000)]
 
-	# for beta in np.linspace(-0.99, 0.99, 3):
-	beta = -0.99
+# for beta in np.linspace(-0.99, 0.99, 3):
+beta = 0.99
 
-	height = np.array([oneoverf.sequence(n, beta, rng, phase_range=phase) for rng in rngs])
-	slope = np.diff(height)
+height = np.array([oneoverf.sequence(n, beta, rng, phase_range=phase) for rng in rngs])
+slope = np.diff(height)
 
-	hist, bin_edges = np.histogram(slope, bins='auto', density=True)
+hist, bin_edges = np.histogram(slope, bins='auto', density=True)
 
 
-	print(f'{hist.size=}')
-	print(hist)
-	print(f'{bin_edges.size=}')
-	print(bin_edges)
+print(f'{hist.size=}')
+print(hist)
+print(f'{bin_edges.size=}')
+print(bin_edges)
 
-	sample_slope = [-1, 0, 1]
-	print(f'prob={probability(sample_slope, slope)}')
+sample_slope = [-1, 0, 1]
+print(f'prob={probability(sample_slope, slope)}')
 
-	print()
+print()
 
-	plt.plot(bin_edges[:-1], hist, label=f'{beta=:2} {n=}')
+plt.plot(bin_edges[:-1], hist, label=f'{beta=:2} {n=}')
 
-	mean, std = norm.fit(slope)
-	x = np.linspace(-20, 20, 100)
-	plt.plot(x, norm.pdf(x, mean, std), label=f'fit {beta:2}')
+mean, std = norm.fit(slope)
+x = np.linspace(-20, 20, 100)
+plt.plot(x, norm.pdf(x, mean, std), label=f'fit {beta:2}')
 
-	print(flush=True)
+print(flush=True)
 
 plt.xlim((-20, 20))
 plt.legend()
 plt.savefig(path.out(f'1f_{phase[0]:.1f}-{phase[1]:.1f}_{beta:2}_slope.png'))
 plt.show()
+
+# prob=[0.23422628 0.32976402 0.23475671]
