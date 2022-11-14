7bea51bf357ad59129aa1527135b51b23af05118
trnciii <doityan223@gmail.com>
2022-11-15 04:47:43 +0900

    see results from web

---
+++ test/view-wiener-khinchin/index.py
@@ -0,0 +1,14 @@
+import os, json, __main__
+
+here, _ = os.path.split(__main__.__file__)
+result = os.path.join(here, '../result/wiener-khinchin')
+
+data = {}
+
+for d in os.listdir(result):
+	if os.path.isdir(os.path.join(result, d)):
+		with open(os.path.join(result, d, 'meta.json')) as f:
+			data[d] = json.load(f)
+
+with open(os.path.join(here, 'index.js'), 'w') as f:
+	f.write(f'const table = {json.dumps(data, indent=2)};')
