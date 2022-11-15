09877585a254a800b4f0716634ff0c6b807c90b3
trnciii <doityan223@gmail.com>
2022-11-15 22:48:06 +0900

    fix directory name

--- test/path.py
+++ test/path.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 def out(file=''):
 	d, f = os.path.split(__main__.__file__)
-	now = str(datetime.now().timestamp()).replace('.','')
+	now = str(datetime.now().timestamp()).replace('.','').ljust(16,'0')
 	p = os.path.join(d, 'result', f.replace('.py', ''), now)
 	os.makedirs(p, exist_ok=True)
 	return os.path.join(p, file)
