1632e9d193d56bf0bbc5a454f2f2814a194324ff
trnciii <doityan223@gmail.com>
2022-11-17 23:23:10 +0900

    condition by slope

--- binding/test/kernel.hpp
+++ binding/test/kernel.hpp
@@ -23,13 +23,22 @@ __device__ float g1_distant(const float* height, size_t realizations, size_t len
 
 __device__ float g1_distant_single(
 	const float* height,
-	const size_t* starts, size_t starts_size,
-	size_t length,
-	float ray_slope)
+	const size_t* starts, const size_t starts_size,
+	const size_t length,
+	const float ray_slope)
 {
 	float visible = starts_size;
+	float condition = starts_size;
+
 	for(size_t m=0; m<starts_size; m++){
-		size_t start = starts[m];
+		const size_t start = starts[m];
+
+		if (height[start+1] - height[start] > ray_slope){
+			condition -= 1;
+			visible -= 1;
+			continue;
+		}
+
 		for(size_t i=0; i<length; i++){
 			if(height[start] + ray_slope*i < height[start + i]){
 				visible -= 1;
@@ -37,7 +46,7 @@ __device__ float g1_distant_single(
 			}
 		}
 	}
-	return visible / starts_size;
+	return visible / condition;
 }
 
 
