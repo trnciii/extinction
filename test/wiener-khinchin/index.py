8636a1c05111e17f1d207127f91ef3ab4711e4f8
trnciii <doityan223@gmail.com>
2022-11-18 21:25:54 +0900

    print exceptions on indexing

--- test/wiener-khinchin/index.py
+++ test/wiener-khinchin/index.py
@@ -4,11 +4,21 @@ here, _ = os.path.split(__main__.__file__)
 result = os.path.join(here, 'result')
 
 data = {}
+error = []
 
 for d in os.listdir(result):
-	if os.path.isdir(os.path.join(result, d)):
-		with open(os.path.join(result, d, 'meta.json')) as f:
-			data[d] = json.load(f)
+	try:
+		if os.path.isdir(os.path.join(result, d)):
+			with open(os.path.join(result, d, 'meta.json')) as f:
+				data[d] = json.load(f)
+	except Exception as e:
+		error.append((d, e))
+
+if len(error)>0:
+	print('Error')
+	for i, e in error:
+		print(i)
+		print(f'\t{e}')
 
 with open(os.path.join(here, 'index.js'), 'w') as f:
 	f.write(f'const table = {json.dumps(data, indent=2)};')
