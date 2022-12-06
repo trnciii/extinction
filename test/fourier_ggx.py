6c621574783a1acbf76236bfc58de96201dd5572
trnciii <doityan223@gmail.com>
2022-12-06 19:52:09 +0900

    update plotting

--- test/fourier_ggx.py
+++ test/fourier_ggx.py
@@ -2,35 +2,32 @@ import numpy as np
 import os
 from matplotlib import pyplot as plt
 
+def plot(x):
+	f, ax = plt.subplots(3, 1, constrained_layout=True, figsize=(25, 15))
+	ft = np.fft.fft(x)
+	amp = np.abs(ft)
+	phase = np.angle(ft)
+	freq = np.fft.fftfreq(x.shape[0])
+
+	ax[0].title.set_text('signal')
+	ax[0].plot(range(x.shape[0]), x)
+
+	ax[1].title.set_text('amp')
+	ax[1].plot(freq, amp)
+
+	ax[2].title.set_text('phase')
+	ax[2].plot(freq, phase)
+
+	return ft, amp, phase, freq
+
+
 path = '../data/ggx-sigma-0-25-0-25_2048-it-150.npy'
-f, ax = plt.subplots(3,1,constrained_layout=True, figsize=(30,20))
-
-
 height = np.load(path)[1024]
 
-print(height)
+_, _, phase, _ = plot(height)
 
-signal = np.fft.fft(height)
-amp = np.abs(signal)
-phase = np.angle(signal)
-freq = np.fft.fftfreq(height.shape[0])
+plot(phase[:200])
 
-print(f'{signal.shape=}')
-half = signal.shape[0]//2
-# half = 200
-
-n = max(2**10, half)
-ret = np.pad(phase[:half], (0, n-half), 'symmetric')
-
-print(f'{phase[:half].shape}')
-print(f'{phase[:half]=}')
-
-print(f'{ret.shape=}')
-print(f'{ret=}')
-
-ax[0].plot(freq, amp)
-ax[1].plot(range(ret.shape[0]), ret)
-ax[2].plot(range(height.shape[0]), height)
 
 print(flush=True)
 plt.show()
