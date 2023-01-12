225536398e2abd07296d3b26e4910d472b56ff9e
trnciii <doityan223@gmail.com>
2023-01-12 12:15:14 +0900

    update figure

--- test/render/compare.py
+++ test/render/compare.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from matplotlib.colors import Normalize
 from PIL import Image
 import numpy as np
 import os
@@ -45,7 +46,7 @@ for alpha, frame in itertools.product(alphas, frames):
 
 	fig, axes = plt.subplots(
 		width+1, width+1,
-		figsize=(20,20),
+		figsize=(15, 15),
 		constrained_layout=True
 	)
 	# plt.subplots_adjust(wspace=0.05, hspace=0.05)
@@ -57,11 +58,15 @@ for alpha, frame in itertools.product(alphas, frames):
 	]
 
 	images = [
-		np.asarray(Image.open(
+		np.array(Image.open(
 			os.path.join(path.from_spec(alpha, t, m), f'frame_{frame}.png')
-		).convert('L'))
+		).convert('F'))
 		for t, m in type_memories
 	]
+
+	for i in images:
+		i /= np.max(i)
+
 
 	axes[0][0].imshow(images[0], cmap='gray')
 	axes[0][0].axis('off')
@@ -69,17 +74,17 @@ for alpha, frame in itertools.product(alphas, frames):
 	for i, (t, m) in enumerate(type_memories):
 		f = load_npy(alpha, t, m, 'height.npy')
 
-		left = f.shape[0]//2 - 50
-		right = left + 100
-		low = np.min(f) - 0.5
-		high = np.max(f) + 0.5
+		left = f.shape[0]//2 - 200
+		right = left + 400
+		low = np.min(f[left:right]) - 15*alpha
+		high = np.max(f[left:right]) + 15*alpha
 
 		for a in [axes[0][1+i], axes[1+i][0]]:
-			a.plot(range(right-left), f[left:right])
+			a.plot(range(right-left), f[left:right], linewidth=1)
 			a.text(
 				0.05, 0.95,
 				f'{rename_table.get(t,t)} ({memory_label[m]})' if t!='white' else 'delta',
-				fontsize=16,
+				fontsize=12,
 				transform = a.transAxes,
 				horizontalalignment='left',
 				verticalalignment='top'
@@ -91,7 +96,11 @@ for alpha, frame in itertools.product(alphas, frames):
 
 	for _a, base in zip(axes[1:], images):
 		for a, i in zip(_a[1:], images):
-			a.imshow((i - base))
 			a.axis('off')
+
+			if base is i: continue
+
+			diff = i - base
+			a.imshow(diff, norm=Normalize(vmin=-0.05, vmax=0.05), cmap='seismic')
 
 	fig.savefig(f'compared/{alpha:3.2f}_{frame}.jpg')
